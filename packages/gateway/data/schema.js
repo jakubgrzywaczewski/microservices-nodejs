d963b4395ec13e547ed58bfcfc3cf26903492f06
enk1 <grzywaczewski.jakub@gmail.com>
2020-04-12 15:18:31 +0200

    mail schema defined for Apollo

--- packages/gateway/data/schema.js
+++ packages/gateway/data/schema.js
@@ -4,7 +4,21 @@ import resolvers from './resolvers.js';
 const { makeExecutableSchema } = graphQlTools;
 
 const typeDefs = `
-  type Query { hey: String! }
+  type Query {
+    Mails: [Mail]
+    mail(subject: String!, receiver: String!): Mail
+  }
+
+  type Mutation {
+    mail(subject: String!, receiver: String!, content: String!): Mail
+  }
+
+  type Mail {
+    subject: String
+    receiver: String
+    content: String
+    _id: String
+  }
 `;
 
 const schema = makeExecutableSchema({
