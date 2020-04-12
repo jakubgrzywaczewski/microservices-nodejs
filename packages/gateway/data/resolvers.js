d3ed0dc5ede1cb73d6d44700171764ed617ab015
enk1 <grzywaczewski.jakub@gmail.com>
2020-04-12 15:25:09 +0200

    resolvers mocked

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,6 +1,29 @@
+const mockMails = [
+  {
+    subject: 'first',
+    receiver: 'test@test.tst',
+    content: 'hi first',
+  },
+  {
+    subject: 'second',
+    receiver: 'test2@test.tst',
+    content: 'hi second',
+  },
+  {
+    subject: 'third',
+    receiver: 'test3@test.tst',
+    content: 'hi third',
+  },
+  {
+    subject: 'firfourthst',
+    receiver: 'test@test.tst',
+    content: 'hi fourth',
+  },
+];
+
 const resolvers = {
   Query: {
-    hey: () => 'hi there',
+    mails: () => mockMails,
   },
 };
 
